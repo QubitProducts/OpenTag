95c69aa13663a4cfe516d5a13da0a714053cf218
Peter Fronc <peter.fronc@qubit.com>
2015-09-18 19:46:10 +0100

    Adding updated compiler.

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ current=$(dirname ${0})
 echo $current
 cd $current
 
+pwd
+
 #fail if failed function 
 exitOnError(){
   errorCode="$?";
@@ -18,12 +20,13 @@ exitOnError(){
 #source scripts...
 
 DEBUGSCRIPT=""
-LOCAL="//= require,//:include"
+LOCAL="//= require,//:include,//:import,//:css"
 LOG="/*L*/,log.FINE(,log.INFO(,log.FINEST(,log.ERROR(,log.WARN(,//L,Log(,qubit.opentag.Log"
 TEST="/*~tests*/"
 NEVER="/*~NEVER INCLUDE*/,/*~TRASH*/"
 SESSION="//:session"
 PURE=",/*NO LOG*/,/*NO EXTRAS-disabled*/"
+COMMON=" --keep-lines"
 
 mkdir build
 
@@ -34,44 +37,27 @@ echo "Generating main.js file..."
 
 
 #merge js
-java -jar bin/MiniMerge.jar --info \
-  -v \
-  -dl "/*D*/,$LOCAL,$LOG,$DEBUGSCRIPT" \
-  -dw "/*~debug*/,/*~log*/,$TEST,$NEVER,/**~ TRASH **/"\
-  -df "/* exclude from merge */,$PURE,/** EXCLUDE FROM NEW API **/,/*NO CONSENT COOKIE*/"\
-  -o build/tagsdk.js \
-  -i .js\
-  -s src/js/qubit\
-  --source-base src/js/
+java -jar bin/compilejs.jar --info \
+ -v \
+ -dl "$LOCAL" \
+ -dw "/**~ TRASH **/,$NEVER"\
+ -df "/* exclude from merge */,/** EXCLUDE FROM NEW API **/,/*NO CONSENT COOKIE*/"\
+ -o build/tagsdk.o \
+ -i .js \
+ -s src/js \
+ --source-base src/js  $COMMON
+
 exitOnError
 
-cat src/templates/prefix > out.tmp
-cat build/tagsdk.js >> out.tmp
-cat src/templates/suffix >> out.tmp
-cp -f out.tmp build/tagsdk.js
-
-#merge js
-java -jar bin/MiniMerge.jar --info \
-  -v \
-  -dl "$LOCAL" \
-  -dw "/**~ TRASH **/,$NEVER"\
-  -df "/* exclude from merge */,/** EXCLUDE FROM NEW API **/,/*NO CONSENT COOKIE*/"\
-  -o build/tagsdk-debug.js \
-  -i .js\
-  -s src/js/qubit\
-  --source-base src/js/
-exitOnError
-
-cat src/templates/prefix > out.tmp
-cat build/tagsdk-debug.js >> out.tmp
-cat src/templates/suffix >> out.tmp
-cp -f out.tmp build/tagsdk-debug.js
-
-rm -f out.tmp
+echo "(function () {\n" > build/tagsdk.js
+cat build/tagsdk.o.js >> build/tagsdk.js
+echo "\n}());" >> build/tagsdk.js
+rm -vf build/tagsdk.o.js
 
 echo "Compressing build/tagsdk.js ..."
 java -jar bin/yuicompressor-2.4.jar build/tagsdk.js -o build/tmp.js
 exitOnError
+
 java -jar bin/compiler.jar \
   --js build/tmp.js \
   --js_output_file build/tagsdk-compressed.js \
@@ -82,15 +68,8 @@ exitOnError
 rm build/tmp.js
 
 
-echo "Compressing build/tagsdk-debug.js ..."
-java -jar bin/yuicompressor-2.4.jar build/tagsdk-debug.js -o build/tmp.js
-exitOnError
-java -jar bin/compiler.jar \
-  --js build/tmp.js \
-  --js_output_file build/tagsdk-debug-compressed.js \
-  --compilation_level SIMPLE\
-  --summary_detail_level 1\
-  --warning_level QUIET
+#merge js
+
 exitOnError
 rm build/tmp.js
 
